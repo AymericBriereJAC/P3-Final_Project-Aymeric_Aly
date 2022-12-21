fb37c0fe4633b46204c0d73bb2655f4cc7f360f7
Alejandro Cadena <2168579@johnabbottcollege.net>
2022-12-21 13:49:57 -0500

    moved stuff

--- FinalProject-Alejandro-Aymeric/FinalProject-Alejandro-Aymeric/obj/Debug/netcoreapp3.1/Receipt.g.i.cs
+++ FinalProject-Alejandro-Aymeric/FinalProject-Alejandro-Aymeric/obj/Debug/netcoreapp3.1/Receipt.g.i.cs
@@ -61,7 +61,7 @@ namespace FinalProject_Alejandro_Aymeric {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/FinalProject-Alejandro-Aymeric;V1.0.0.0;component/receipt.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FinalProject-Alejandro-Aymeric;component/receipt.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Receipt.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
